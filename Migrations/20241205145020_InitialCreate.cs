2e776bf03ba6daec5462940ee5446661744bae07
home <nuttapong_coach@hotmail.com>
2024-12-05 22:03:27 +0700

    first commit

---
+++ Migrations/20241205145020_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VideoGameApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoGames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Platform = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Developer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoGames", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "VideoGames",
+                columns: new[] { "Id", "Developer", "Platform", "Publisher", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Insomniac Games", "PS5", "Sony Interactive Entertainment", "spider-Man 2" },
+                    { 2, "Nintendo EPD", "Nintendo Switch", "Nintendo", "The Legend of Zelda: Breath of the Wild" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoGames");
+        }
+    }
+}
