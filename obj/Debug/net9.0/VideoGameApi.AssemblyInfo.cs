71e54609d18eeb71e53927a991c245ca9aee2cb9
somsak <anonymous8155@gmail.com>
2024-12-06 10:34:03 +0700

    demo2

--- obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/VideoGameApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be37ce9cb19d966d0b76b429da88bc317cfd23b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64c94d493e589108a3f9daaea656b5d35b490fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
